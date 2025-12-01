c97f478bb40c21202a7cb4ff458970f564434e73
Ant1xi <antcastroguerrero@gmail.com>
2025-12-01 14:26:13 +0100

    Proyecto base Spring Boot

---
+++ src/main/java/com/antonio/nominas/NominasSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.nominas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominasSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NominasSpringBootApplication.class, args);
+    }
+
+}
